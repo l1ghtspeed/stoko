87c12ec3760a34f441cfae7625ed1e7fc898aaba
Zi Gao <zi.s.gao@gmail.com>
2021-01-27 17:54:59 -0500

    push to kafka topic

--- producers/twitter/app.py
+++ producers/twitter/app.py
@@ -1,13 +1,21 @@
 import os
 import tweepy
+from json import dumps
+from kafka import KafkaProducer
 from stream import StreamListener
 
-API_KEY, API_SECRET = os.environ['twitter_api_key'], os.environ['twitter_api_secret']
+
 ACCESS_TOKEN, ACCESS_SECRET = os.environ['twitter_access_token'], os.environ['twitter_access_secret']
 auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
 api = tweepy.API(auth)
 
-stream_listener = StreamListener()
+
+producer = KafkaProducer(
+            bootstrap_servers=[os.environ['kafka_bootstrap_server']],
+            value_serializer=lambda x: dumps(x).encode('utf-8')
+          )
+
+stream_listener = StreamListener(producer)
 stream = tweepy.Stream(auth = api.auth, listener=stream_listener)
 stream.filter(track=['bitcoin'])
--- producers/twitter/stream.py
+++ producers/twitter/stream.py
@@ -1,9 +1,12 @@
 import tweepy
-
 class StreamListener(tweepy.StreamListener):
+    def __init__(self, producer):
+        self.producer = producer
+        super().__init__()
 
     def on_status(self, status):
-        print(status.text)
+        data = {'tweet': status.text}
+        self.producer.send('numtest', value=data)
         
     def on_error(self, status_code):
         if status_code == 420:
